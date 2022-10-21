9f4c932a55130cffe858626f81b385ca81eabf5d
jothmani <jo.tlaesus.it@gmail.com>
2022-10-21 22:03:28 +0200

    changes done

--- src/main/java/org/jothmart/testing/MathUtils.java
+++ src/main/java/org/jothmart/testing/MathUtils.java
@@ -5,6 +5,8 @@ public class MathUtils {
         return a+b;
     }
 
+    // This has to be added
+    //TODO revuiew code
     public double computeCircleRadius(double radius){
         return Math.PI * radius * radius;
     }
